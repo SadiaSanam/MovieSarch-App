e736cf38c9d6ff037560265db0dc82c538c1a1c5
SadiaSanam <72416961+SadiaSanam@users.noreply.github.com>
2021-02-23 17:48:55 -0500

    part 1
    1. working animations
    2. form setups.
    to do:
    1.add API and continue to refine

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Searchmovie from './components/Searchmovie';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='title' id='startShow'>Movie Search</h1>
+      <Searchmovie />
     </div>
   );
 }
---
+++ src/components/Searchmovie.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Searchmovie = () => {
+  return (
+    <div id= 'content' className='section'>
+      <form className='form-control'>
+        <label className='form-label'> Movie Name </label>
+        <input className='form-input' type= 'text' name='query' required />
+        <button className='form-button' type='submit'> Search </button>
+      </form>
+      
+    </div>
+  )
+}
+
+export default Searchmovie
