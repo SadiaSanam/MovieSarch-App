8909bfca688c24975a9298cf94820ca012165e6b
SadiaSanam <72416961+SadiaSanam@users.noreply.github.com>
2021-02-24 15:11:03 -0500

    movie app done.
    viewable contents.
    to do:
    deploy using firebase

--- src/components/Searchmovie.js
+++ src/components/Searchmovie.js
@@ -1,14 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import MovieInfo from './MovieInfo';
 
 const Searchmovie = () => {
+  const[ query, setQuery ] = useState('');
+  const [ movies, setMovies ] = useState( [] ); 
+
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=84ba351b547c6ffd4b9391b0eea2a0cf&language=en-US&query=${query}&page=1&include_adult=false`;
+
+  const getMovies = async (e) => {
+   try {
+    e.preventDefault();
+    const result = await fetch (url);
+    const data = await result.json();
+
+    setMovies(data.results );
+
+    console.log('my states', query, movies)
+   } catch (error) {
+     console.log('fetch error', error.message)
+    }  
+  }
+
+
   return (
-    <div id= 'content' className='section'>
+    <div id= 'content' className='section' onSubmit= {getMovies} >
       <form className='form-control'>
         <label className='form-label'> Movie Name </label>
-        <input className='form-input' type= 'text' name='query' required />
+        <input className='form-input' type= 'text' name='query' required 
+          onChange={ e => setQuery(e.target.value)}/>
         <button className='form-button' type='submit'> Search </button>
       </form>
-      
+
+      <div className='card-list'>
+
+      </div>
+      {
+        movies.filter(movie => movie.poster_path ).map( movie => 
+        <MovieInfo movie={movie} key={movie.id}/>
+          )
+      }
+
     </div>
   )
 }
